9bee08988d2f2dd0a9c7a5f2eb8aab952d553093
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-11-22 10:25:38 +0100

    Exercice 3 : Validation des entrées utilisateur

---
+++ WebApplication1/ApiApplication1/Queries/GetWeatherByDateQuery.cs
@@ -0,0 +1,12 @@
+﻿using ApiApplication1.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiApplication1.Queries
+{
+    public class GetWeatherByDateQuery
+    {
+        [Required]
+        [WeatherDateValidation]
+        public DateOnly Date { get; set; }
+    }
+}
---
+++ WebApplication1/ApiApplication1/ValidationAttributes/WeatherDateValidationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiApplication1.ValidationAttributes
+{
+    public class WeatherDateValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not DateOnly date)
+            {
+                return new ValidationResult(ErrorMessage ?? "La date n'est pas au bon format");
+            }
+
+            if (date < DateOnly.FromDateTime(DateTime.Now) || date > DateOnly.FromDateTime(DateTime.Now.AddDays(5)))
+            {
+                return new ValidationResult(ErrorMessage ?? "La date ne peut pas dépasser 5 jours à partir d'aujourd'hui");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
