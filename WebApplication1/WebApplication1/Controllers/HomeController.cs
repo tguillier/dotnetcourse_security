e2f962a6d4351ace77ef361f4f5c505efbe839a8
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-11-23 15:38:12 +0100

    Exercice 6 : Protéger les données sensibles avec ASP.NET Core Data Protection.

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApplication1.Models;
@@ -6,11 +9,17 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IDataProtectionProvider _dataProtectionProvider;
+        private readonly IDataProtector _protector;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            IDataProtectionProvider dataProtectionProvider,
+            UserManager<IdentityUser> userManager)
         {
-            _logger = logger;
+            _dataProtectionProvider = dataProtectionProvider;
+            _protector = dataProtectionProvider.CreateProtector(nameof(HomeController));
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -27,6 +36,40 @@ namespace WebApplication1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> InitCookie()
+        {
+            string cookieProtectedData = "SensitiveData";
+            string? cookieContent = Request.Cookies[cookieProtectedData];
+
+            if (string.IsNullOrEmpty(cookieContent))
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                string protectedData = _protector.Protect(user.Email);
+                Console.WriteLine($"Email : {user.Email}");
+                Console.WriteLine($"Email protégé : {protectedData}");
+
+                Response.Cookies.Append(
+                    cookieProtectedData,
+                    protectedData,
+                    new CookieOptions
+                    {
+                        Secure = true,
+                        HttpOnly = true,
+                        MaxAge = TimeSpan.FromHours(1)
+                    });
+            }
+            else
+            {
+                string unprotectedCookie = _protector.Unprotect(cookieContent);
+                Console.WriteLine($"Contenu du cookie protégé : {cookieContent}");
+                Console.WriteLine($"Contenu du cookie : {unprotectedCookie}");
+            }
+
+            return LocalRedirect("/");
         }
     }
 }
